e64edbb8aa7c01badebbbc5e682793e53fec7201
sofia <sofia.jimlap@educa.jcyl.es>
2023-10-03 16:58:46 +0200

    start

--- app/src/main/java/com/iesam/androidtrainning/MainActivity.kt
+++ app/src/main/java/com/iesam/androidtrainning/MainActivity.kt
@@ -2,10 +2,22 @@ package com.iesam.androidtrainning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("@dev", "onStrat ejecutandose")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("@dev", "onResume ejecutandose")
+    }
 }
+
