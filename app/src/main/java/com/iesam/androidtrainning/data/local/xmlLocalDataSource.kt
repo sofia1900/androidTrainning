4633af428bb62c91ac461cc622b4d94941d9c37c
sofia <sofia.jimlap@educa.jcyl.es>
2023-10-04 19:53:13 +0200

    start

---
+++ app/src/main/java/com/iesam/androidtrainning/data/local/xmlLocalDataSource.kt
@@ -0,0 +1,34 @@
+package com.iesam.androidtrainning.data.local
+
+import android.content.Context
+import com.iesam.androidtrainning.app.ErrorApp
+import com.iesam.kotlintrainning.Either
+import com.iesam.kotlintrainning.left
+import com.iesam.kotlintrainning.right
+
+class xmlLocalDataSource (private val context : Context){
+                                                          //nombre del fichero
+    private val sharedPref = context.getSharedPreferences("users", Context.MODE_PRIVATE)
+                                                                    //Si es accesible desde otra app
+
+    //https://developer.android.com/training/data-storage/shared-preferences?hl=es-419
+    fun saveUser(userName : String , surname : String) : Either<ErrorApp, Boolean> {
+        /*val editor = sharedPref.edit()
+        editor.putString("username", userName)
+        editor.putString("surname", surname)
+        editor.apply()
+        sharedPref.edit()*/
+
+        try{
+            with(sharedPref.edit()){
+                putString("username", userName)
+                putString("surname", surname)
+                apply()
+            }
+            return true.right()
+        }catch (ex : Exception){
+            return ErrorApp.UnknowError.left()
+        }
+
+    }
+}
