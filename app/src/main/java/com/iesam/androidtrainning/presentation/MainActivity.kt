a28b71a91112d2d435051b20d16e6e274bedf2cf
sofia1900 <sofia.jimlap@educa.jcyl.es>
2023-10-06 10:40:01 +0200

    get

--- app/src/main/java/com/iesam/androidtrainning/presentation/MainActivity.kt
+++ app/src/main/java/com/iesam/androidtrainning/presentation/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.iesam.androidtrainning.presentation
 
+import android.database.Observable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.lifecycle.Observer
 import com.iesam.androidtrainning.R
 import com.iesam.androidtrainning.data.UserDataRepository
 import com.iesam.androidtrainning.data.local.xmlLocalDataSource
+import com.iesam.androidtrainning.domain.GetUserUseCase
 import com.iesam.androidtrainning.domain.SaveUserUseCase
 import com.iesam.androidtrainning.domain.User
 
@@ -14,13 +17,16 @@ class MainActivity : AppCompatActivity() {
 
     val viewModels : MainViewModel  by lazy{
         MainViewModel (
-            SaveUserUseCase(UserDataRepository(xmlLocalDataSource(this)))
+            SaveUserUseCase(UserDataRepository(xmlLocalDataSource(this))),
+            GetUserUseCase(UserDataRepository(xmlLocalDataSource(this)))
         )
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
         setupView()
+        setupObservers()
+
     }
 
 
@@ -40,5 +46,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun getAgeInput() : String =
         findViewById<EditText>(R.id.input_age).text.toString()
+
+
+    private fun setupObservers(){
+        val observer = Observer<MainViewModel.UiState>{
+            it.user?.apply {
+                bindData(this)
+            }
+        }
+        viewModels.uiState.observe(this, observer)
+    }
+
+    private fun bindData (user : User){
+        setNameInput(user.username)
+        setSurnameInput(user.surname)
+        setAgeInput(user.age)
+    }
+
+    private fun setNameInput(name: String) {
+        findViewById<EditText>(R.id.input_name).setText(name)
+    }
+
+    private fun setSurnameInput(surname: String) {
+        findViewById<EditText>(R.id.input_surname).setText(surname)
+    }
+
+    private fun setAgeInput(age: String) {
+        findViewById<EditText>(R.id.input_age).setText(age)
+    }
 }
 
--- app/src/main/java/com/iesam/androidtrainning/presentation/MainViewModel.kt
+++ app/src/main/java/com/iesam/androidtrainning/presentation/MainViewModel.kt
@@ -1,19 +1,37 @@
 package com.iesam.androidtrainning.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.iesam.androidtrainning.app.ErrorApp
+import com.iesam.androidtrainning.domain.GetUserUseCase
 import com.iesam.androidtrainning.domain.SaveUserUseCase
+import com.iesam.androidtrainning.domain.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainViewModel (private val saveUserUseCase : SaveUserUseCase) : ViewModel(){
+class MainViewModel (private val saveUserUseCase : SaveUserUseCase,
+    private val getUserUseCase: GetUserUseCase) : ViewModel(){
+
+    private val _uiState = MutableLiveData<UiState>()
+    val uiState : LiveData<UiState> = _uiState
+
 
     fun saveUser(name: String, surname: String, date: String) {
         saveUserUseCase(SaveUserUseCase.Input(name, surname, date)).fold(
             { responseError(it) },
             { responseSuccess(it) }
         )
+    }
+
+    fun getUser (){
+        viewModelScope.launch(Dispatchers.IO) {
+            getUserUseCase().fold(
+                {responseError(it)},
+                {responseGetUserSuccess(it)}
+            )
+        }
     }
 
     private fun responseError (errorApp: ErrorApp){
@@ -23,4 +41,14 @@ class MainViewModel (private val saveUserUseCase : SaveUserUseCase) : ViewModel(
     private fun responseSuccess(isOk : Boolean){
 
     }
+
+    private fun responseGetUserSuccess(user : User){
+        _uiState.postValue(UiState(user = user))
+    }
+
+    data class UiState (
+        val errorApp: ErrorApp? = null,
+        val isLoading : Boolean = false,
+        val user : User? = null
+    )
 }
